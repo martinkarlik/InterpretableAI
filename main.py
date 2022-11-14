b1e2933a0d2f0641e0f343b328fe25cb4c2a9722
Martin Karlik <karlik.m97@gmail.com>
2022-11-14 15:37:56 +0100

    Simplified the project.

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from time import time
 from keras import callbacks
 from timeit import default_timer as timer
 import model
+import dataset
 
-import utils
-
-
-X_train, y_train, X_test, y_test, X_val, y_val = utils.get_dataset_reshaped(seed=100)
+X_train, y_train, X_test, y_test, X_val, y_val = dataset.get_dataset_reshaped(seed=100)
 
 net = model.CNN_model()
 
